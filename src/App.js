e5eca6a94a34c0e753c4aecc74767fe17d4bba76
cyanchill <anthonyliang9@gmail.com>
2021-10-05 22:16:23 -0400

    utilized react routing to choose what's being displayed on the page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
-import Profile from "./components/Profile";
+import "./styles/App.scss";
+import Container from "react-bootstrap/Container";
+
+/* For page routing */
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import NavBar from "./components/NavBar";
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
+import Classes from "./components/pages/Classes";
+import Profile from "./components/pages/Profile";
 
 const App = () => {
   return (
-    <div>
-      <h1>GradSchoolZero</h1>
-      <Profile />
-    </div>
+    <Router>
+      <NavBar />
+      <Container>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/classes">
+            <Classes />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/profile">
+            <Profile />
+          </Route>
+        </Switch>
+      </Container>
+    </Router>
   );
 };
 
