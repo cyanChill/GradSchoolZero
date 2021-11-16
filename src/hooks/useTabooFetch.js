cb8c8cd053e5c393926e67c291c798f8d62ab266
cyanChill <anthonyliang9@gmail.com>
2021-11-15 21:10:29 -0500

    implemented funcs to count taboo words & censor taboo words in a string

--- src/hooks/useTabooFetch.js
+++ src/hooks/useTabooFetch.js
@@ -2,10 +2,31 @@ import { useState, useEffect } from "react";
 
 const useTabooFetch = () => {
   const [tabooList, setTabooList] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   /* Function to count number of taboo words in the inputted text */
+  const countTabooWords = (str) => {
+    let cnt = 0;
+
+    tabooList.forEach((word) => {
+      const regex = new RegExp(word, "ig");
+      cnt += (str.match(regex) || []).length;
+    });
+
+    return cnt;
+  };
 
   /* Function to censor all taboo words in the inputted text */
+  const censorTabooWords = (str) => {
+    let censorStr = str;
+
+    tabooList.forEach((word) => {
+      const regex = new RegExp(word, "ig");
+      censorStr = censorStr.replace(regex, "*".repeat(word.length));
+    });
+
+    return censorStr;
+  };
 
   const addTabooWord = async (word) => {
     const formattedWord = word.toLowerCase();
@@ -48,19 +69,28 @@ const useTabooFetch = () => {
   };
 
   const refreshTabooList = async () => {
+    setLoading(true);
     const res = await fetch(`http://localhost:2543/tabooWords`);
     const data = await res.json();
 
     if (data.length > 0) {
       setTabooList(data[0].tabooList);
     }
+    setLoading(false);
   };
 
   useEffect(() => {
     refreshTabooList();
   }, []);
 
-  return { tabooList, addTabooWord, deleteTabooWord };
+  return {
+    tabooList,
+    loading,
+    addTabooWord,
+    deleteTabooWord,
+    countTabooWords,
+    censorTabooWords,
+  };
 };
 
 export default useTabooFetch;
