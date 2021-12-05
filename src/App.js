1af08446e1d5e628a8f1267917392f8db142eca8
cyanChill <anthonyliang9@gmail.com>
2021-12-05 13:00:13 -0500

    prepared application for a footer

--- src/App.js
+++ src/App.js
@@ -37,71 +37,73 @@ const App = () => {
     <Router>
       <NavBar />
 
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/allcourses" component={AllCourses} />
-        <Route path="/allcourses/:id" component={CourseHub} />
-        <Route exact path="/courses" component={Courses} />
-        <Route path="/courses/:id" component={CoursePage} />
-        <Route path="/about" component={About} />
-        <Route path="/students" component={Students} />
-        <Route path="/instructors" component={Instructors} />
+      <div className="offset-height">
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/allcourses" component={AllCourses} />
+          <Route path="/allcourses/:id" component={CourseHub} />
+          <Route exact path="/courses" component={Courses} />
+          <Route path="/courses/:id" component={CoursePage} />
+          <Route path="/about" component={About} />
+          <Route path="/students" component={Students} />
+          <Route path="/instructors" component={Instructors} />
 
-        <ProtectedRoute exact path="/profile">
-          <Profile />
-        </ProtectedRoute>
-        <Route path="/profile/:id" component={Profile} />
+          <ProtectedRoute exact path="/profile">
+            <Profile />
+          </ProtectedRoute>
+          <Route path="/profile/:id" component={Profile} />
 
-        <Route path="/logout" component={Logout} />
+          <Route path="/logout" component={Logout} />
 
-        {/* Not logged in users only*/}
-        <PublicRoute path="/apply">
-          <ApplyPage />
-        </PublicRoute>
-        <PublicRoute path="/login">
-          <Login />
-        </PublicRoute>
+          {/* Not logged in users only*/}
+          <PublicRoute path="/apply">
+            <ApplyPage />
+          </PublicRoute>
+          <PublicRoute path="/login">
+            <Login />
+          </PublicRoute>
 
-        {/* Logged in users only */}
-        <ProtectedRoute path="/settings">
-          <Settings />
-        </ProtectedRoute>
+          {/* Logged in users only */}
+          <ProtectedRoute path="/settings">
+            <Settings />
+          </ProtectedRoute>
 
-        {/* Registrar users only */}
-        <RegistrarRoute path="/registrar">
-          <RegistrarMangementPage />
-        </RegistrarRoute>
-        <RegistrarRoute path="/create/user">
-          <CreateUserForm />
-        </RegistrarRoute>
-        <RegistrarRoute path="/create/course">
-          <CreateCourseForm />
-        </RegistrarRoute>
-        <RegistrarRoute exact path="/applications">
-          <ApplicationsPage />
-        </RegistrarRoute>
-        <RegistrarRoute path="/applications/:id">
-          <Applicant />
-        </RegistrarRoute>
-        <RegistrarRoute path="/taboo">
-          <ManageTaboo />
-        </RegistrarRoute>
-        <RegistrarRoute path="/semester">
-          <SemesterManagement />
-        </RegistrarRoute>
-        <RegistrarRoute exact path="/infractions">
-          <InfractionsPage />
-        </RegistrarRoute>
-        <RegistrarRoute exact path="/complaints">
-          <ComplaintsPage />
-        </RegistrarRoute>
-        <RegistrarRoute exact path="/grad-apps">
-          <GradAppsPage />
-        </RegistrarRoute>
+          {/* Registrar users only */}
+          <RegistrarRoute path="/registrar">
+            <RegistrarMangementPage />
+          </RegistrarRoute>
+          <RegistrarRoute path="/create/user">
+            <CreateUserForm />
+          </RegistrarRoute>
+          <RegistrarRoute path="/create/course">
+            <CreateCourseForm />
+          </RegistrarRoute>
+          <RegistrarRoute exact path="/applications">
+            <ApplicationsPage />
+          </RegistrarRoute>
+          <RegistrarRoute path="/applications/:id">
+            <Applicant />
+          </RegistrarRoute>
+          <RegistrarRoute path="/taboo">
+            <ManageTaboo />
+          </RegistrarRoute>
+          <RegistrarRoute path="/semester">
+            <SemesterManagement />
+          </RegistrarRoute>
+          <RegistrarRoute exact path="/infractions">
+            <InfractionsPage />
+          </RegistrarRoute>
+          <RegistrarRoute exact path="/complaints">
+            <ComplaintsPage />
+          </RegistrarRoute>
+          <RegistrarRoute exact path="/grad-apps">
+            <GradAppsPage />
+          </RegistrarRoute>
 
-        {/* Page not found */}
-        <Route path="*" component={NotFound} />
-      </Switch>
+          {/* Page not found */}
+          <Route path="*" component={NotFound} />
+        </Switch>
+      </div>
     </Router>
   );
 };
